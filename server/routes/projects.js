6ccbc3915ec08256e7dd1b7c57b8d2f2fb814fc7
Naveen Jangam <jangamnaveen291@gmail.com>
2025-08-05 19:28:08 +0530

    Final fix: Corrected swapped questions and projects routes

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -1,31 +1,31 @@
 const express = require('express');
 const router = express.Router();
-const questionController = require('../controllers/questionController'); // Correct controller
+const projectController = require('../controllers/projectController'); // Correct controller
 const auth = require('../middleware/auth');
 
-// @route   POST api/questions
-// @desc    Create a question
+// @route   POST api/projects
+// @desc    Create a project
 // @access  Private
-router.post('/', auth, questionController.createQuestion);
+router.post('/', auth, projectController.createProject);
 
-// @route   GET api/questions
-// @desc    Get all questions
+// @route   GET api/projects
+// @desc    Get all projects
 // @access  Public
-router.get('/', questionController.getAllQuestions);
+router.get('/', projectController.getAllProjects);
 
-// @route   GET api/questions/:id
-// @desc    Get question by ID
+// @route   GET api/projects/:id
+// @desc    Get project by ID
 // @access  Public
-router.get('/:id', questionController.getQuestionById);
+router.get('/:id', projectController.getProjectById);
 
-// @route   POST api/questions/:id/answers
-// @desc    Add an answer to question
+// @route   POST api/projects/:id/comments
+// @desc    Add a comment to project
 // @access  Private
-router.post('/:id/answers', auth, questionController.addAnswer);
+router.post('/:id/comments', auth, projectController.addComment);
 
-// @route   PUT api/questions/:id/upvote
-// @desc    Upvote a question
+// @route   PUT api/projects/:id/like
+// @desc    Like a project
 // @access  Private
-router.put('/:id/upvote', auth, questionController.upvoteQuestion);
+router.put('/:id/like', auth, projectController.likeProject);
 
 module.exports = router;
